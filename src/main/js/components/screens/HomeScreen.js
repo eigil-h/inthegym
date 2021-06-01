2031ec2ebb1e6af51ba836cd239c901b6aac927e
eigil-h <eigil.hysver@protonmail.com>
2021-06-01 10:37:47 +0200

    workout data retrieved from firebase realtime database

--- src/main/js/components/screens/HomeScreen.js
+++ src/main/js/components/screens/HomeScreen.js
@@ -1,25 +1,33 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Text, Pressable, View} from "react-native";
 import STYLES, {pressableStyle} from '../../constants/styles'
-import WORKOUT from "../../data/hardboiled"
+import {loadHome} from "../../data/firebase"
 
-const HomeScreen = ({navigation}) => (
-  <View style={STYLES.screen}>
-    {Object.keys(WORKOUT).map(title =>
-      <Pressable
-        key={title}
-        onPress={() => navigation.navigate('Workout', {
-          title,
-          exercises: WORKOUT[title]
-        })}
-        style={pressableStyle}
-      >
-        <View style={STYLES.butt}>
-          <Text style={STYLES.buttTxt}>{title}</Text>
-        </View>
-      </Pressable>
-    )}
-  </View>
-);
+const HomeScreen = ({navigation}) => {
+  const [workouts, setWorkouts] = useState([]);
+
+  useEffect(() => {
+    loadHome(setWorkouts);
+  }, []);
+
+  return (
+    <View style={STYLES.screen}>
+      {Object.keys(workouts).map(title =>
+        <Pressable
+          key={title}
+          onPress={() => navigation.navigate('Workout', {
+            title,
+            exercises: workouts[title]
+          })}
+          style={pressableStyle}
+        >
+          <View style={STYLES.butt}>
+            <Text style={STYLES.buttTxt}>{title}</Text>
+          </View>
+        </Pressable>
+      )}
+    </View>
+  )
+};
 
 export default HomeScreen
---
+++ src/main/js/data/firebase.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase';
+import 'firebase/database';
+
+firebase.initializeApp({
+  databaseURL: 'https://in-the-gym-default-rtdb.europe-west1.firebasedatabase.app/',
+  projectId: 'in-the-gym'
+});
+
+export const loadHome = (setter) => {
+  const dbRef = firebase.database().ref();
+  dbRef.child('1').get().then((snapshot) => {
+    if (snapshot.exists()) {
+      setter(snapshot.val());
+    }
+  });
+};
